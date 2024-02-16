37df2aaa654a4e0cf3e0ae92d984fb15399a8310
joseph.spearhead@gmail.com <ofodumjoseph@yahoo.com>
2024-02-16 05:24:44 +0100

    the restart quiz button

--- quiz_app/lib/question_screen.dart
+++ quiz_app/lib/question_screen.dart
@@ -1,61 +1,129 @@
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+// import 'package:flutter/material.dart';
+// import 'package:quiz_app/answer_button.dart';
+// import 'package:quiz_app/data/questions.dart';
+
+
+// class QuestionsScreen extends StatefulWidget {
+//   const QuestionsScreen({super.key, required this.onSeletcAnswer});
+
+//   final void Function (String answer) onSeletcAnswer;
+
+//   @override
+//   State<QuestionsScreen> createState() {
+//     return _QuestionScreenState();
+//   }
+// }
+
+// class _QuestionScreenState extends State<QuestionsScreen> {
+//   var currentQuestionIndex = 0;
+
+//   void answerQuestion(String selectedAnswers){
+//     widget.onSeletcAnswer(selectedAnswers);
+//     setState(() {
+//         // currentQuestionIndex = currentQuestionIndex + 1;
+//     currentQuestionIndex += 1;
+//     // currentQuestionIndex++;
+      
+//     });
+//   }
+//   @override
+//   Widget build(context) {
+//     final currentQuestion = questions[currentQuestionIndex];
+//     return SizedBox(
+//       width: double.infinity,
+//       child: Container(
+//         margin: const EdgeInsets.all(40),
+//         child: Column(
+//             // another alternative to the center widget that we have been using
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             children: [
+//               Text(
+//                 currentQuestion.text,
+//                 style: GoogleFonts.lato(
+//                   color: const Color.fromARGB(255, 203, 144, 144),
+//                   fontSize: 24,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//                 textAlign: TextAlign.center,
+//               ),
+//               const SizedBox(height: 30),
+//               ...currentQuestion.getShuffledAnswers().map((answer) {
+//                 return AnswerButton(answerText: answer, onTap: () {
+//                   answerQuestion(answer);
+//                 });
+//               })
+//             ]),
+//       ),
+//     );
+//   }
+// }
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
 
-
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key, required this.onSeletcAnswer});
+  const QuestionsScreen({
+    super.key,
+    required this.onSelectAnswer,
+  });
 
-  final void Function (String answer) onSeletcAnswer;
+  final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
-    return _QuestionScreenState();
+    return _QuestionsScreenState();
   }
 }
 
-class _QuestionScreenState extends State<QuestionsScreen> {
+class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void answerQuestion(String selectedAnswers){
-    widget.onSeletcAnswer(selectedAnswers);
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
+    // currentQuestionIndex = currentQuestionIndex + 1;
+    // currentQuestionIndex += 1;
     setState(() {
-        // currentQuestionIndex = currentQuestionIndex + 1;
-    currentQuestionIndex += 1;
-    // currentQuestionIndex++;
-      
+      currentQuestionIndex++; // increments the value by 1
     });
   }
+
   @override
   Widget build(context) {
     final currentQuestion = questions[currentQuestionIndex];
+
     return SizedBox(
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(40),
         child: Column(
-            // another alternative to the center widget that we have been using
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                currentQuestion.text,
-                style: GoogleFonts.lato(
-                  color: const Color.fromARGB(255, 203, 144, 144),
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 201, 153, 251),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 30),
-              ...currentQuestion.getShuffledAnswers().map((answer) {
-                return AnswerButton(answerText: answer, onTap: () {
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {
                   answerQuestion(answer);
-                });
-              })
-            ]),
+                },
+              );
+            })
+          ],
+        ),
       ),
     );
   }
--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -1,70 +1,150 @@
+// import 'package:flutter/material.dart';
+// import 'package:quiz_app/question_screen.dart';
+// import 'package:quiz_app/result_screen.dart';
+// import 'package:quiz_app/start_screen.dart';
+// import 'package:quiz_app/data/questions.dart';
+
+// class Quiz extends StatefulWidget {
+
+// const Quiz ({super.key});
+
+//   @override
+//   State<Quiz> createState() {
+//     return _QuizState();
+    
+//   }
+// }
+
+// class _QuizState extends State<Quiz>{
+//  List<String> selectedAnswers = [];
+//  var activeScreen = 'start-screen';
+
+// switchScreen(){
+//   setState(() {
+//     activeScreen = 'questions-Screen';
+//   });
+// }
+
+// void chooseAnswer(String answer){
+//   selectedAnswers.add(answer);
+
+//   if(selectedAnswers.length == questions.length){
+//     setState(() {
+//       activeScreen = 'results-screen';
+//     });
+//   }
+// }
+//   @override
+//   Widget build(context){
+
+//     Widget screenWidget = StartScreen(switchScreen); 
+
+//     if(activeScreen == 'questions-Screen'){
+//       screenWidget = QuestionsScreen(
+//         onSeletcAnswer: chooseAnswer,);
+//     }
+
+//     if (activeScreen == 'results-screen'){
+//       screenWidget = ResultsScreen(
+//         chosenAnswers: selectedAnswers,);
+//     }
+
+//     return  MaterialApp(
+//       home: Scaffold(
+//         body: Container(
+//           decoration: const BoxDecoration(
+//             gradient: LinearGradient(
+//               colors: [
+//                 Color.fromARGB(255, 51, 7, 126),
+//                 Color.fromARGB(255, 108, 65, 183),
+//               ],
+//               begin: Alignment.topLeft,
+//               end: Alignment.bottomRight,
+//             ),
+           
+//           ),
+//           child:screenWidget,
+//           ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
+
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/question_screen.dart';
 import 'package:quiz_app/result_screen.dart';
 import 'package:quiz_app/start_screen.dart';
-import 'package:quiz_app/data/questions.dart';
 
 class Quiz extends StatefulWidget {
-
-const Quiz ({super.key});
+  const Quiz({super.key});
 
   @override
   State<Quiz> createState() {
     return _QuizState();
-    
   }
 }
 
-class _QuizState extends State<Quiz>{
- List<String> selectedAnswers = [];
- var activeScreen = 'start-screen';
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
 
-switchScreen(){
-  setState(() {
-    activeScreen = 'questions-Screen';
-  });
-}
-
-void chooseAnswer(String answer){
-  selectedAnswers.add(answer);
-
-  if(selectedAnswers.length == questions.length){
+  void switchScreen() {
     setState(() {
-      activeScreen = 'results-screen';
+      activeScreen = 'questions-screen';
     });
   }
-}
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
   @override
-  Widget build(context){
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
 
-    Widget screenWidget = StartScreen(switchScreen); 
-
-    if(activeScreen == 'questions-Screen'){
+    if (activeScreen == 'questions-screen') {
       screenWidget = QuestionsScreen(
-        onSeletcAnswer: chooseAnswer,);
+        onSelectAnswer: chooseAnswer,
+      );
     }
 
-    if (activeScreen == 'results-screen'){
+    if (activeScreen == 'results-screen') {
       screenWidget = ResultsScreen(
-        chosenAnswers: selectedAnswers,);
+        chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
+      );
     }
 
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Color.fromARGB(255, 51, 7, 126),
-                Color.fromARGB(255, 108, 65, 183),
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
               ],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
-           
           ),
-          child:screenWidget,
-          ),
+          child: screenWidget,
+        ),
       ),
     );
   }
